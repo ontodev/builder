eda1c87d34604aeb98ff93dd777b8b3784ff3c5b
Nikola <nikola.peric03@gmail.com>
2016-09-10 08:39:49 -0400

    Args to new command for adding modular elements available by default such as tasks

--- src/ontodev/builder/new.clj
+++ src/ontodev/builder/new.clj
@@ -7,6 +7,10 @@
             [boot.core :as boot]
             [boot.util :as util]))
 
+(def ^:const default-modules
+  {:tasks {:ns    'ontodev.builder.tasks.view
+           :views 'ontodev.builder.tasks.view/config}})
+
 (defn readme
   [name]
   (format
@@ -15,22 +19,41 @@
 A new project using [BUILDer](http://github.com/ontodev/builder)."
    name))
 
+(defn module-ns-str
+  [module-kw]
+  (if-let [module-ns (get-in default-modules [module-kw :ns])]
+    (format "'[%s]" module-ns)
+    (throw (RuntimeException. (format "'%s' is not an available default module" module-kw)))))
+
+(defn module-views [modules]
+  (into []
+        (comp
+          (map (fn [kw]
+                 (get-in default-modules [kw :views])))
+          (filter identity))
+        modules))
+
 (defn build-boot
-  [name]
+  [modules name]
   (format
    "(set-env!
   :resource-paths #{\"src\"}
   :dependencies '[[ontodev/builder \"0.1.0-SNAPSHOT\"]])
 
-(require '[ontodev.builder.builder :refer :all])
+(require '[ontodev.builder.builder :refer :all]
+         %s)
 
 (def builder-project
-  {:project \"%s\"})
-" name))
+  {:project \"%s\"
+   :views %s})
+" (apply str (map module-ns-str modules))
+   name
+   (module-views modules)))
 
 (boot/deftask new
   "Create a new BUILDer project."
-  [n name NAME str "generated project name"]
+  [n name NAME str "generated project name"
+   m modules MODULES edn "builder modules to include"]
   (cond
     (not (string? name))
     (throw (RuntimeException. "--name is required"))
@@ -38,10 +61,13 @@ A new project using [BUILDer](http://github.com/ontodev/builder)."
     (.exists (io/file name))
     (throw (RuntimeException. (format "'%s' already exists" name)))
 
+    (and modules (not (coll? modules)))
+    (throw (RuntimeException. "--modules must be formatted as a collection"))
+
     :else
     (do
       (.mkdir (io/file name))
       (.mkdir (io/file name "src"))
       (spit (io/file name "README.md") (readme name))
-      (spit (io/file name "build.boot") (build-boot name))
+      (spit (io/file name "build.boot") (build-boot modules name ))
       (println (format "Created project directory '%s'" name)))))
