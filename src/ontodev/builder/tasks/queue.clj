cb32c86461bdcc29898900a1eb6d7e363db2c3e0
James A. Overton <james@overton.ca>
2016-09-14 08:21:24 -0700

    Rework queue and tests
    - change terminology: tasks and executions
    - keep all history
    - add docstrings
    - update tests, remove some tests

--- src/ontodev/builder/tasks/queue.clj
+++ src/ontodev/builder/tasks/queue.clj
@@ -1,40 +1,92 @@
 (ns ontodev.builder.tasks.queue)
 
+;; A `built.boot` file specifies a number of tasks.
+;; A task can be executed multiple times
+;; (although we currently allow just one execution at a time).
+;; We keep a history of all executions during the current session
+;; (no persistence between sessions).
+
 ;; TODO: potentially use mount for state management
-(def ran-tasks (atom {}))
+(def execution-history (atom (sorted-map)))
 
-(defn add-task
-  [{task-fn   :var
-    task-name :name}]
-  (if (and (ifn? task-fn)
-           (not (coll? task-fn))
-           (or (string? task-name)
-               (symbol? task-name)))
-    (swap! ran-tasks assoc (System/currentTimeMillis) {:task   task-name
-                                                       :result (future (task-fn))})
-    (throw (RuntimeException. "Malformed task attempted to be added"))))
+(defn show-execution
+  "Given an execution map,
+   return with status and dereferenced results."
+  [{:keys [f] :as execution}]
+  (when (and execution (future? f))
+    (assoc
+     (dissoc execution :f)
+     :status
+     (cond
+       (future-cancelled? f) :cancelled
+       (future-done? f) :done
+       :else :pending)
+     :result
+     (when (and (future-done? f)
+                (not (future-cancelled? f)))
+       @f))))
 
-(defn drop-task
+(defn get-execution
+  "Given a valid execution ID,
+   return a dereferenced execution map."
   [id]
-  (swap! ran-tasks dissoc id))
+  (when-let [execution (get @execution-history id)]
+    (show-execution execution)))
 
-(defn get-task-future
-  [id]
-  (->> id
-       (get @ran-tasks)
-       :result))
+(defn get-executions
+  "Get all executions in the history."
+  []
+  (map show-execution (vals @execution-history)))
 
-(defn get-task
-  [id]
-  (when-let [f (get-task-future id)]
-    (cond
-      (future-cancelled? f) "Task cancelled!"
-      (future-done? f) @f
-      :else "Task diligently being processed")))
+(defn get-current-execution
+  "Return the first execution in the history that is still pending, or nil."
+  []
+  (->> @execution-history
+       vals
+       (filter #(not (future-done? (:f %))))
+       first
+       show-execution))
 
-(defn cancel-task
+(defn execute-task
+  "Given a valid Boot task,
+   if nothing else is currently executing,
+   start a new execution, add it to the history,
+   and return a dereferenced execution map."
+  [task]
+  (cond
+    (not (map? task))
+    (throw (RuntimeException. "Task is malformed"))
+
+    (not (string? (:name task)))
+    (throw (RuntimeException. "Task name not provided"))
+
+    (not (ifn? (:var task)))
+    (throw (RuntimeException. "Task function (var) not provided"))
+
+    (get-current-execution)
+    (throw (RuntimeException. "Another task is already executing"))
+
+    :else
+    (let [id (System/currentTimeMillis)]
+      (swap!
+       execution-history
+       assoc
+       id
+       {:id id
+        :name (:name task)
+        :f (future ((:var task)))})
+      (get-execution id))))
+
+(defn cancel-execution
+  "Given a valid execution ID,
+   if it is still pending, cancel it."
   [id]
-  (when-let [f (get-task-future id)]
+  (when-let [f (get-in @execution-history [id :f])]
     (when-not (future-done? f)
-      (future-cancel f)
-      (drop-task id))))
+      (future-cancel f))))
+
+(defn cancel-all-executions
+  "Cancel all executions in the history."
+  []
+  (doseq [id (keys execution-history)]
+    (cancel-execution id)))
--- test/ontodev/builder/tasks/queue_test.clj
+++ test/ontodev/builder/tasks/queue_test.clj
@@ -2,93 +2,43 @@
   (:require [clojure.test :refer :all]
             [ontodev.builder.tasks.queue :refer :all]))
 
-(defn clear-ran-tasks-fixture
-  [f]
-  (reset! ran-tasks {})
-  (f))
+(def test-task {:var (fn [] "test task") :name "Best Task"})
+(def long-task {:var (fn [] (Thread/sleep 5000) "long task") :name "Long Task"})
 
-(use-fixtures :each clear-ran-tasks-fixture)
-
-(def test-task-result "Result")
-
-(def test-task {:var (fn [] test-task-result) :name "Best Task"})
-(def long-task {:var (fn [] (Thread/sleep 5000)) :name "Long Task"})
-
-(defn first-tasks-key
-  []
-  (-> @ran-tasks
-      first
-      key))
-
-(deftest test-add-task
-  (testing "Add a task and see it in the ran-tasks"
-    (add-task test-task)
-    (is (not-empty @ran-tasks)))
-
+(deftest test-execute-task
   (testing "Attempt to add a malformed task and fail"
-    (is (thrown-with-msg? RuntimeException
-                          #"Malformed task attempted to be added"
-                          (add-task "")))
-    (is (thrown-with-msg? RuntimeException
-                          #"Malformed task attempted to be added"
-                          (add-task {:var  ""
-                                     :name nil}))))
+    (is (thrown-with-msg?
+         RuntimeException
+         #"Task is malformed"
+         (execute-task "")))
+    (is (thrown-with-msg?
+         RuntimeException
+         #"Task name not provided"
+         (execute-task
+          {:var  ""
+           :name nil}))))
 
-  (testing "Accept ifn as var params"
-    (is (add-task {:var #'first-tasks-key :name "asdf"})))
+  (testing "Accept task"
+    (let [execution (execute-task test-task)
+          id        (:id execution)]
+      (is (= execution
+             (get-execution id)
+             {:id id
+              :name "Best Task"
+              :status :done
+              :result "test task"})))))
 
-  (testing "Accept symbol as task name"
-    (is (add-task {:var (fn [] "asdf") :name 'test-task}))))
-
-(deftest test-drop-task
-  (testing "Drop the first task given its id"
-    (add-task test-task)
-    (let [k (first-tasks-key)]
-      (drop-task k)
-      (is (nil? (get @ran-tasks k))))))
-
-(deftest test-get-task-future
-  (testing "Ensures a future is returned for a task"
-    (add-task test-task)
-    (is (future? (get-task-future (first-tasks-key)))))
-
-  (testing "Get a nonexistent task"
-    (is (nil? (get-task-future 123)))))
-
-(deftest test-get-task
-  (testing "Get a completed task"
-    (add-task test-task)
-    (let [k (first-tasks-key)]
-      (= (get-task k) test-task-result)))
-
-  (testing "Get a cancelled task"
-    (add-task long-task)
-    (let [k (first-tasks-key)]
-      (cancel-task k)
-      (= (get-task k) "Task cancelled!")))
-
-  (testing "Get an unfinished task"
-    (add-task long-task)
-    (let [k (first-tasks-key)]
-      (= (get-task k) "Task diligently being processed")))
-
-  (testing "Get a nonexistent task"
-    (is (nil? (get-task 123)))))
-
-(deftest test-cancel-task
-  (testing "Cancel a running task"
-    (add-task long-task)
-    (let [k (first-tasks-key)]
-      (cancel-task k)
-      (is (nil? (get-task k)))))
-
-  (testing "Do not cancel a task if complete"
-    (add-task test-task)
-    (let [k (first-tasks-key)]
-      @(get-task-future k)
-      (cancel-task k)
-      (is (get @ran-tasks k))
-      (is (not (future-cancelled? (get-task-future k))))))
-
-  (testing "Do not cancel a task if it does not exist"
-    (is (nil? (cancel-task 123)))))
+(deftest test-cancel-execution
+  (testing "Execute a task then cancel execution"
+    (is (nil? (get-current-execution)))
+    (let [execution (execute-task long-task)
+          id (:id execution)]
+      (is (not (nil? (get-current-execution))))
+      (is (= :pending (:status (get-execution id))))
+      (cancel-execution id)
+      (is (nil? (get-current-execution)))
+      (is (= (get-execution id)
+             {:id id
+              :name "Long Task"
+              :status :cancelled
+              :result nil})))))
